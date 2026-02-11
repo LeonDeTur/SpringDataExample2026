dc56fab5df2dd7b9fd2bb42336057925dd0a5de0
Leon <100690100+LeonDeTur@users.noreply.github.com>
2026-02-11 15:01:37 +0300

    fix commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from test_package import add
+
+if __name__ == "__main__":
+    print(test.add(2, 2))
---
+++ test_package/test.py
@@ -0,0 +1,10 @@
+def add(a: int, b: int) -> int:
+    """Add one int to another
+    Args:
+        a (int): num 1
+        b (int): num 2
+    Returns:
+        int: result a + b
+    """
+
+    return a + b
